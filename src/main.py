5b0d1bf6f3f66bff41dd5726e576086e18867b07
Bruno R <31624845+Arachnight@users.noreply.github.com>
2024-06-21 06:49:22 -0400

    Ajout variables environnements (#10)
    * added dotenv module
    
    * added environment variables and unit tests
    
    * added environment variables in main program
    
    * changed small thing in readme about env var setup

--- src/main.py
+++ src/main.py
@@ -1,24 +1,25 @@
-from signalrcore.hub_connection_builder import HubConnectionBuilder
-import logging
-import requests
 import json
+import logging
 import time
+from signalrcore.hub_connection_builder import HubConnectionBuilder
+import requests
+from env_variables import EnvVariables
 
 
 class App:
     def __init__(self):
         self._hub_connection = None
-        self.TICKS = 10
+        self.ticks = 10
 
         # To be configured by your team
-        self.HOST = None  # Setup your host here
-        self.TOKEN = None  # Setup your token here
-        self.T_MAX = None  # Setup your max temperature here
-        self.T_MIN = None  # Setup your min temperature here
-        self.DATABASE_URL = None  # Setup your database here
+        self.host = EnvVariables.get_host()
+        self.token = EnvVariables.get_token()
+        self.t_max = EnvVariables.get_t_max()
+        self.t_min = EnvVariables.get_t_min()
+        self.database_url = EnvVariables.get_db_url()
 
     def __del__(self):
-        if self._hub_connection != None:
+        if self._hub_connection is not None:
             self._hub_connection.stop()
 
     def start(self):
@@ -33,7 +34,7 @@ class App:
         """Configure hub connection and subscribe to sensor data events."""
         self._hub_connection = (
             HubConnectionBuilder()
-            .with_url(f"{self.HOST}/SensorHub?token={self.TOKEN}")
+            .with_url(f"{self.host}/SensorHub?token={self.token}")
             .configure_logging(logging.INFO)
             .with_automatic_reconnect(
                 {
@@ -65,14 +66,14 @@ class App:
 
     def take_action(self, temperature):
         """Take action to HVAC depending on current temperature."""
-        if float(temperature) >= float(self.T_MAX):
+        if float(temperature) >= float(self.t_max):
             self.send_action_to_hvac("TurnOnAc")
-        elif float(temperature) <= float(self.T_MIN):
+        elif float(temperature) <= float(self.t_min):
             self.send_action_to_hvac("TurnOnHeater")
 
     def send_action_to_hvac(self, action):
         """Send action query to the HVAC service."""
-        r = requests.get(f"{self.HOST}/api/hvac/{self.TOKEN}/{action}/{self.TICKS}")
+        r = requests.get(f"{self.host}/api/hvac/{self.token}/{action}/{self.ticks}")
         details = json.loads(r.text)
         print(details, flush=True)
 
